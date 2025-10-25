54e4f39b8dad50f96141ddf73867630c86206162
TIMOFEY <johndoe@example.com>
2025-10-25 16:59:26 +0600

    start

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# start-all-with-host.sh
+
+# Запускаем все серверы с командой host
+screen -S mindAI -dm bash -c "cd /root/mindAI && java -jar server-release.jar"
+sleep 1
+screen -S mindAI -X stuff "host\n"
+
+screen -S mindNoTurret -dm bash -c "cd /root/mindNoTurret && java -jar server-release.jar"
+sleep 1
+screen -S mindNoTurret -X stuff "host\n"
+
+screen -S mindSurv -dm bash -c "cd /root/mindSurv && java -jar server-release.jar"
+sleep 1
+screen -S mindSurv -X stuff "host\n"
+
+screen -S mindZeroHit -dm bash -c "cd /root/mindZeroHit && java -jar server-release.jar"
+sleep 1
+screen -S mindZeroHit -X stuff "host\n"
+
+echo "Все серверы запущены с командой 'host'"
+screen -ls
